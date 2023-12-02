1b63582aa7c4c9490c1038722e1f986b716fa5c4
KrishanthaBSomarathna <info.krishantha.b@gmail.com>
2023-12-02 14:57:46 +0530

    SL Traveler

---
+++ app/src/main/java/com/example/sl_travelar/Home.java
@@ -0,0 +1,23 @@
+package com.example.sl_travelar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+}
